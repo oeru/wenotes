0e12e5b9240aafcfbf6018e07d2936704ac063bb
Jim Tittsler <jimt@onjapan.net>
2016-03-07 05:49:15 +0000

    Add [WEnotesPost] shortcode

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
-WEnotes plugin for WordPress
-====
+# WEnotes plugin for WordPress
 
 This is a very quick rendering of
 [WikiEducator Widget:WEnotes](http://WikiEducator.org/Widget:WEnotes)
-as a WordPress plugin. It adds a single shortcode with the same two
-(optional) arguments as the widget.
+and [WikiEducator Widget:WEnotesPost](http://WikiEducator.org/Widget:WEnotesPost)
+as a WordPress plugin. It adds shortcodes similar arguments to the widgets.
 
-[WEnotes tag="wikieducator" count="20"]
+* [WEnotes tag="wikieducator" count="20"]
+* [WEnotesPost tag="wikieducator" button="Post a WEnote"]
+
+## Dependencies
+
+The posting side of the plugin requires local copies (easily installed
+as git submodules) of:
+
+* [WEnotes](https://bitbucket.org/wikieducator/wenotes), the client-side
+Javascript
+* [Sag](http://saggingcouch.com/), CouchDB libraries for PHP and Javascript
+(the PHP one is used here)
 
--- WEnotes.php
+++ WEnotes.php
@@ -5,8 +5,8 @@
 /*
 Plugin Name: WEnotes
 Plugin URI: http://github.com/oeru/wenotes
-Description: Display an aggregated WEnotes stream.
-Version: 1.0.1
+Description: Display and post to an aggregated WEnotes stream.
+Version: 1.1.0
 Author: Jim Tittsler
 Author URI: http://WikiEducator.org/User:JimTittsler
 License: GPLv2 or later
@@ -28,12 +28,15 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+define( 'WENOTES_VERSION', '1.1.0' );
+
 if ( !function_exists( 'add_action' ) ) {
 	echo 'This only works as a WordPress plugin.';
 	exit;
 }
 
 add_shortcode( 'WEnotes', 'wenotes_func' );
+add_shortcode( 'WEnotesPost', 'wenotespost' );
 
 function wenotes_func( $atts ) {
 	$a = shortcode_atts( array(
@@ -61,3 +64,80 @@ EOD;
 	return $wenotesdiv;
 }
 
+function wenotespost( $atts ) {
+	$a = shortcode_atts( array(
+	    'tag' => '',
+	    'button' => 'Post a WEnote',
+	    'anonymous' => ''
+	), $atts );
+	$current_user = wp_get_current_user();
+	if ( $current_user->ID == 0 ) {
+		$wenotespostdiv = '';
+		if ( $a['anonymous'] ) {
+			$wenotespostdiv = '<div><p>' . $a['anonymous'] . '</p></div>';
+		}
+	} else {
+		wp_enqueue_script( 'wenotespostwp',
+			plugins_url( 'wenotes/WEnotesPostWP.js', __FILE__ ),
+			array( 'jquery' ),
+			WENOTES_VERSION,
+			true );
+		$wenotespostdiv = <<<EOD
+<div id="WEnotesPost1"></div>
+<script type="text/javascript">/*<![CDATA[*/
+$ = window.jQuery;
+$(function() {
+  WEnotesPostWP("WEnotesPost1", 'ds4oer', '${a['button']}', '53');
+})/*]]>*/</script>
+EOD;
+	}
+	return $wenotespostdiv;
+}
+
+add_action( 'wp_ajax_wenotes', 'wenotespost_ajax' );
+function wenotespostresponse( $a ) {
+	echo json_encode( $a );
+	die();
+}
+
+function wenotespost_ajax() {
+	require_once( 'sag/src/Sag.php' );
+
+	$current_user = wp_get_current_user();
+	list( $usec, $ts ) = explode( ' ', microtime() );
+        $sag = new Sag( WENOTES_HOST, WENOTES_PORT );
+        $sag->setDatabase( WENOTES_DB );
+        $sag->login( WENOTES_USER, WENOTES_PASS );
+
+        $data = array(
+                'from_user' => $current_user->user_login,
+                'from_user_name' => $current_user->display_name,
+                'created_at' => date( 'r', $ts ),
+                'text' => trim($_POST['notext']),
+                'id' => $current_user->ID . $ts . substr( "00000$usec", 0, 6 ),
+                'we_source' => 'course',
+                'we_tags' => array( trim($_POST['notag']) ),
+                'we_timestamp' => date('Y-m-d\TH:i:s.000\Z', $ts)
+        );
+        if ( $current_user->user_email ) {
+                $data['gravatar'] = md5( strtolower( trim( $current_user->user_email ) ) );
+        }
+        if ( $current_user->user_url ) {
+                $data['profile_url'] = $current_user->user_url;
+        }
+        if ( isset( $_POST['we_page'] ) ) {
+                $data['we_page'] = $_POST['we_page'];
+        }
+        if ( isset( $_POST['we_root'] ) ) {
+                $data['we_root'] = $_POST['we_root'];
+        }
+        if ( isset( $_POST['we_parent'] ) ) {
+                $data['we_parent'] = $_POST['we_parent'];
+        }
+
+        $sag->post( $data );
+
+	wenotespostresponse( array(
+		'posted' => true
+	));
+}
