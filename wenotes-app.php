2b88de114dc5faf1a4dc1ec7b42f3819c5da7f95
Dave Lane <dave@oerfoundation.org>
2018-03-06 07:20:14 +0000

    returning WEnotesClient.js back to production by default!

--- wenotes-app.php
+++ wenotes-app.php
@@ -857,8 +857,8 @@ EOD;
     $(function() {
         if (!window.WEnotes) {
             window.WEnotes = true;
-           // $.getScript('//wikieducator.org/extensions/WEnotes/WEnotes-min.js');
-            $.getScript('//c.wikieducator.org/extensions/WEnotes/WEnotesClient.js');
+            $.getScript('//wikieducator.org/extensions/WEnotes/WEnotes-min.js');
+           // $.getScript('//c.wikieducator.org/extensions/WEnotes/WEnotesClient.js');
         }
 })/*]]>*/</script>
 EOD;
